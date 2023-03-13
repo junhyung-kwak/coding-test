0c0fa480e2472b3fc2897f5f81feeb81afc8a335
junhyung-kwak <junhk0914@gmail.com>
2023-03-14 03:13:13 +0900

    argparse

--- main.py
+++ main.py
@@ -3,19 +3,25 @@ from solution import solution
 if __name__ == "__main__":
     from glob import glob
     import time
+    import argparse 
     # load inputs
     from utils import loader
-    site = "programmers"
 
-    loader = getattr(loader, site)
+    parser = argparse.ArgumentParser(description="coding-test",)
+    parser.add_argument("--site", type=str, default="programmers", choices=["programmers",])
+    parser.add_argument("--split", type=str, default="\t", )
+    args = parser.parse_args()
+
+    loader = getattr(loader, args.site)
 
     for file in glob('./test_case/*.txt'):
-        inputs = loader(file)
+        inputs = loader(file, args.split)
         st = time.time()
         ret = solution(*inputs[:-1])
         et = time.time()
         
-        print('|| test start', file, '||')
-        print('sol :', ret,f'== {inputs[-1]} ? ' f'{(et-st)*1000:.4f} ms')
-        print('|| test end', file, '||')
+        print('* test start', file, )
+        print('| sol :', ret, ', target :', f'{inputs[-1]} ')
+        print(f'| result :', f'{(ret== inputs[-1])},',  f'{(et-st)*1000:.4f} ms')
+        print('| test end', file, )
         print('\n')
--- utils/loader.py
+++ utils/loader.py
@@ -1,7 +1,7 @@
 
-def programmers(file):
+def programmers(file, split='\t'):
     with open(file, 'r')  as f:
         content = f.read().strip()
 
-    return list(map(eval, content.split('\t')))
+    return list(map(eval, content.split(split)))
 
